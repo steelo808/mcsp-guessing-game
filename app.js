75a571a5f7235fbb4b37ec890534c3c53c215eb9
Sterling Newsom <sterlingnewsom808@gmail.com>
2022-11-07 14:04:35 -0800

    New features added

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-play(); // This is your main function that runs when the page loads
-
+let secretNumber = Math.floor(Math.random() * 10);
+// This is your main function that runs when the page loads
+console.log('Working');
 function play(){
-   
+for(let i = 0 ;i < Infinity; i++){
+    let guess = Number(prompt('Guess a number'));
+    if(guess < secretNumber){
+        alert('Too low guess again');
+    }else if(guess > secretNumber){
+        alert('too high guess again');
+    }else{
+        alert("Correct!");
+        break;
+    }
 }
+}
+play(); 
 
 function playAgain(){
     
