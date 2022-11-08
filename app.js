d5add4a6bbffd76f990eae4f8e72454e2ddc62a2
Sterling Newsom <sterlingnewsom808@gmail.com>
2022-11-08 09:14:01 -0800

    New feature

--- app.js
+++ app.js
@@ -1,16 +1,46 @@
-let secretNumber = Math.floor(Math.random() * 10);
-// This is your main function that runs when the page loads
+let name = prompt('What is your name?');
+let previousScores = {};
+;// This is your main function that runs when the page loads
 console.log('Working');
 function play(){
-for(let i = 0 ;i < Infinity; i++){
+    let secretNumber = Math.floor(Math.random() * 10);
+    let count = 0;
+    let history =[];
+/////edge cases/////////////////////
+// if(Number.isNaN(guess)){
+//     alert ('Only numbers allowed');
+// }else if(guess === Null){
+//     alert('Goodbye!');
+// }
+/////////////END////////////////////
+while(true){
     let guess = Number(prompt('Guess a number'));
+        
+    if(guess === null){
+            alert('Goodbye!');
+            break;
+        }
+
     if(guess < secretNumber){
-        alert('Too low guess again');
+        count++;
+        history.push(guess);
+        alert('Sorry ' + name +
+        ' too low guess again');
+
     }else if(guess > secretNumber){
-        alert('too high guess again');
+        count++;
+        history.push(guess);
+        alert('Sorry ' + name +
+        ' too high guess again');
+
     }else{
-        alert("Correct!");
-        break;
+        alert("Correct! " + "good job " + name + " it took you " + count + " tries! your previous guess(es) were " + history.join(', ') + "!");
+
+        const playAgain = prompt('Do you want to play again?');
+
+        if(playAgain.toLowerCase() === 'Yes'){
+            play();
+        }
     }
 }
 }
